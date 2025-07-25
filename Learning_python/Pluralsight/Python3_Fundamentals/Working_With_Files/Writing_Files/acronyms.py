6cf83327dc5879ff3c322c80236606cdf3329970
Jay Durham <106792001+jdurham2009@users.noreply.github.com>
2025-07-25 14:23:21 -0400

    update

--- Learning_python/Pluralsight/Python3_Fundamentals/Working_With_Files/Writing_Files/acronyms.py
+++ Learning_python/Pluralsight/Python3_Fundamentals/Working_With_Files/Writing_Files/acronyms.py
@@ -1,11 +1,27 @@
-look_up = input("What software acronym would you like to look up?\n")
+def find_acronym():
+    look_up = input("What software acronym would you like to look up?\n")
 
-found = False
-with open("acronyms.txt") as file:
-    for line in file:
-        if look_up in line:
-            print(line)
-            found = True
-            break
-if not found:
-    print("The acronym does not exist")
+    found = False
+    with open("acronyms.txt") as file:
+        for line in file:
+            if look_up in line:
+                print(line)
+                found = True
+                break
+    if not found:
+        print("The acronym does not exist")
+
+def add_acronym():
+    acronym = input("What acronym do you want to add?\n")
+    definition = input("What definition do you want to add?\n")
+    with open("acronyms.txt", "a") as file:
+        file.write(acronym + " - " + definition + "\n")
+
+def main():
+    choice = input("Do you want to find(F) or add(A) an acronym?\n")
+    if choice == "F":
+        find_acronym()
+    elif choice == "A":
+        add_acronym()
+
+main()
