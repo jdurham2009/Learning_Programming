c8200382c6600ff54a8eff180908c8b9f4ec5634
Jay Durham <106792001+jdurham2009@users.noreply.github.com>
2025-07-25 13:56:55 -0400

    add writing files

--- Learning_python/Pluralsight/Python3_Fundamentals/Working_With_Files/Writing_Files/temp.py
+++ Learning_python/Pluralsight/Python3_Fundamentals/Working_With_Files/Writing_Files/temp.py
@@ -1,9 +1,4 @@
-# Ask what acronym to add
-# Ask user for Definition
-#Open file
-#Write to file
-
 acronym = input("What acronym do you want to add?\n")
 definition = input("What definition do you want to add?\n")
-with open(""acronym.txt", "a") as file:
-          file.write(acronym + " - " + definition + "\n")
+with open("acronyms.txt", "a") as file:
+    file.write(acronym + " - " + definition + "\n")
